f13c72e30d9e0251f76596a56183dbe128b1b336
rosindubh <p.welsby2@ntlworld.com>
2021-09-01 10:22:39 +0100

    added setTimeout to componentDidMount / added is statement to render()

--- src/App.js
+++ src/App.js
@@ -3,32 +3,40 @@ import './App.css';
 
 class App extends React.Component {
   state ={
-    data: []
+    data: [],
+    loading: true
   }
 
 handleFetch = async () => {
   const response = await fetch("https://api.adviceslip.com/advice")
   const data = await response.json()
-  console.log(data)
+  //console.log(data)
   this.setState({ data: data.slip  })
 }
 
 componentDidMount() {
   this.handleFetch()
+  setTimeout(() => {
+    this.setState({loading: false})
+  }, 2000)
   console.log("component did mount")
 }
 
 componentDidUpdate () {
+  console.log("------------------")
   console.log("component did update")
 }
 
-
   render() {
-    const { data } = this.state
+    const { data, loading } = this.state
+
+    if(loading) {
+      return<h1>loading...</h1>
+    }
     return (
       <div>
         <h1>hello there</h1>
-        <h3>{ data.advice ? data.advice : "loading..."}</h3>
+        <h3>{ data.advice }</h3>
         <button onClick={this.handleFetch}>get data</button>
       </div>
     )
