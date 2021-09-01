ed7eef8d8c22fbca5cb672076a7a74569eaf3a4a
rosindubh <p.welsby2@ntlworld.com>
2021-09-01 08:03:00 +0100

    set data to be data.slip and extract advice - display as H3

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ handleFetch = async () => {
   const response = await fetch("https://api.adviceslip.com/advice")
   const data = await response.json()
   console.log(data)
+  this.setState({ data: data.slip  })
 }
 
   render() {
     return (
       <div>
         <h1>hello there</h1>
+        <h3>{this.state.data.advice}</h3>
         <button onClick={this.handleFetch}>get data</button>
       </div>
     )
